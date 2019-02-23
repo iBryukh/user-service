a967828d15792f2dab18672830a3dc9f512d3492
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-02-23 12:08:31 +0200

    Add repository layer
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/smarthouse/user/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.smarthouse.user.repository;
+
+import com.smarthouse.user.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/smarthouse/user/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.smarthouse.user.repository;
+
+import com.smarthouse.user.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
