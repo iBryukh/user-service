dbd2a945740ffa1b0415997df9a65e5730d5bdee
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-02-24 15:44:17 +0200

    Add sql query for create tables
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/resources/create_query.sql
@@ -0,0 +1,30 @@
+drop table if exists customer;
+drop table if exists role;
+
+create table role
+(
+  id           bigint       not null,
+  name         varchar(255) not null,
+  access_level integer      not null
+    constraint role_access_level_check
+      check ((access_level <= 3) AND (access_level >= 0)),
+  primary key (id)
+);
+
+create table customer
+(
+  id           bigint       not null,
+  email        varchar(255) not null,
+  name         varchar(255) not null,
+  password     varchar(255) not null,
+  phone_number varchar(255) not null,
+  role_id      int          not null,
+  foreign key (role_id) references role(id),
+  primary key (id)
+);
+
+alter table role
+  owner to postgres;
+
+alter table customer
+  owner to postgres;
