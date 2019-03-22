7af2fdd60a8e2062bdb166bacf39fa2f3b430c28
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-23 00:02:45 +0200

    Add put user request
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/smarthouse/user/controller/UserController.java
+++ src/main/java/com/smarthouse/user/controller/UserController.java
@@ -2,12 +2,11 @@ package com.smarthouse.user.controller;
 
 import com.smarthouse.commonutil.exceptions.ResourceNotFound;
 import com.smarthouse.user.entity.User;
+import com.smarthouse.user.service.DeviceService;
 import com.smarthouse.user.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -18,17 +17,25 @@ import static com.smarthouse.commonutil.exceptions.ResourceNotFound.getNoResourc
 public class UserController {
 
     private UserServiceImpl userService;
+    private DeviceService deviceService;
 
     @Autowired
-    public UserController(final UserServiceImpl userService) {
+    public UserController(final UserServiceImpl userService, final DeviceService deviceService) {
         this.userService = userService;
+        this.deviceService = deviceService;
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
         Optional<User> userOptional = userService.getById(id);
         return ResponseEntity.ok(userOptional.orElseThrow((Supplier<RuntimeException>) () ->
                 new ResourceNotFound(getNoResourceMessage("User", id))
         ));
     }
+
+    @PutMapping
+    public ResponseEntity<User> putUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.save(user));
+    }
+
 }
