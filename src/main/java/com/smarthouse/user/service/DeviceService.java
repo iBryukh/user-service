6f5d3a67b4cef3510a7f03c624b762189a0b2717
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-22 23:40:01 +0200

    Add device service
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/smarthouse/user/service/DeviceService.java
@@ -0,0 +1,25 @@
+package com.smarthouse.user.service;
+
+import com.smarthouse.commonutil.entities.Device;
+import com.smarthouse.user.clients.DeviceRestTemplateClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DeviceService {
+
+    private DeviceRestTemplateClient deviceRestTemplateClient;
+
+    @Autowired
+    public DeviceService(final DeviceRestTemplateClient deviceRestTemplateClient) {
+        this.deviceRestTemplateClient = deviceRestTemplateClient;
+    }
+
+    public Optional<Device> getDevice(final long deviceId) {
+        return deviceRestTemplateClient.getDevice(deviceId);
+    }
+
+
+}
