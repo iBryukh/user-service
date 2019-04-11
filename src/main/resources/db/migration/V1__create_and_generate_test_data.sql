8eb82d82fb9a366db8aab356e22495cbb5869b19
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 20:07:23 +0300

    Add customer_device data generation
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/resources/db/migration/V1__create_and_generate_test_data.sql
+++ src/main/resources/db/migration/V1__create_and_generate_test_data.sql
@@ -2,10 +2,40 @@ DROP TABLE IF EXISTS customer_device CASCADE;
 
 CREATE TABLE IF NOT EXISTS customer_device
 (
-  customer_id integer NOT NULL,
-  device_id   integer NOT NULL,
-  CONSTRAINT customer_device_pk PRIMARY KEY (customer_id, device_id)
+    customer_id integer NOT NULL,
+    device_id   integer NOT NULL,
+    CONSTRAINT customer_device_pk PRIMARY KEY (customer_id, device_id)
 );
 
 ALTER TABLE customer_device
-  OWNER TO postgres;
+    OWNER TO postgres;
+
+-- GENERATE DATA
+
+TRUNCATE TABLE customer_device CASCADE;
+
+DO $$
+    DECLARE
+        freeCustomerId          BIGINT := 0;
+        freeDeviceId            BIGINT := 0;
+        currDevicesCount        INT    := 0;
+        customersCount          INT    := 10;
+        devicesPerCustomerCount INT    := 10;
+        crossDevicesCount       INT    := 2;
+    BEGIN
+        WHILE freeCustomerId < customersCount
+            LOOP
+                if (freeDeviceId > crossDevicesCount) then
+                    freeDeviceId = freeDeviceId - crossDevicesCount;
+                end if;
+                WHILE currDevicesCount < devicesPerCustomerCount
+                    LOOP
+                        INSERT INTO customer_device (customer_id, device_id) VALUES (freeCustomerId, freeDeviceId);
+                        freeDeviceId = freeDeviceId + 1;
+                        currDevicesCount = currDevicesCount + 1;
+                    END LOOP;
+                currDevicesCount = 0;
+                freeCustomerId = freeCustomerId + 1;
+            END LOOP;
+        create sequence customer_device_id_seq start with 100; -- customers count * devicesPerCustomer count
+    END $$;
